15df77d2c0d3561aac4b596b70cf38569b336cd5
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-10-12 06:30:22 +0600

    study on jQuery Selector and text Manipulation

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // document.querySelector("h1").innerHTML="Hello I'm Saikat";
 
-$("h1").text("Good Bye");
+// $("h1").text("Good Bye");
+
+
+// Selector and text Manipulation
+
+$("h1").text("Hello, Change heading text outside a div. ");
+
+$(".my-div h1").text("Hello, Change heading text inside a div.");
+
+$("#p1").text("Hello, Change paragraph text outside a div.");
+
