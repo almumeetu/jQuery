933d8899c5aeb2220c4972245d7ac93df9f5174c
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-10-12 06:53:52 +0600

    complete jquery selector and text manipulation

--- main.js
+++ main.js
@@ -5,9 +5,34 @@
 
 // Selector and text Manipulation
 
+
+// .text("");
+
 $("h1").text("Hello, Change heading text outside a div. ");
 
 $(".my-div h1").text("Hello, Change heading text inside a div.");
 
 $("#p1").text("Hello, Change paragraph text outside a div.");
 
+// $("#p1,h1").text("Hello, Change All");
+
+
+// .html("");
+$("#p1").html("<b>Hello, Change paragraph <br> text outside a div.</b>");
+
+// .append("");
+$(".my-div h1").append(" hi");
+
+
+// .prepend("");
+$("#p1").prepend(" hi");
+
+
+
+var mypara1 = $("<p> </p>").text("This is paragraph 0")
+
+// after
+$("#p2").after(mypara1)
+
+// before
+$("#p1").before(mypara1)
